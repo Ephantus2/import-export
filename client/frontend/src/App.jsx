2ed8dbc60c3793815a8b1b0e66dfa1f3ec591fb9
Ephantus2 <ephantusmwago6@gmail.com>
2025-12-11 17:31:52 +0300

    add routing

--- client/frontend/src/App.jsx
+++ client/frontend/src/App.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import Header from './Header'
 import LandingPage from './LandingPage'
 import Footer from './Footer'
+import { Outlet } from 'react-router-dom'
 
 
 const App = () => {
   return (
     <>
     <Header />
-    <LandingPage />
+    <Outlet />
     <Footer />
     </>
   )
--- client/frontend/src/main.jsx
+++ client/frontend/src/main.jsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import LandingPage from './LandingPage.jsx'
+
+const routers = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {path: '/', element: <LandingPage />}
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={routers} />
   </StrictMode>,
 )
